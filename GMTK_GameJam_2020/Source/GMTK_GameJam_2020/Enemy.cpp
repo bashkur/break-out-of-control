ace5157a4fb3efefa0acbaa0d13e59f5e4b0f31a
Shawn Turk <shawnturk33@gmail.com>
2020-07-12 12:44:43 -0400

    Forgot to stage all changes oops

--- GMTK_GameJam_2020/Source/GMTK_GameJam_2020/Enemy.cpp
+++ GMTK_GameJam_2020/Source/GMTK_GameJam_2020/Enemy.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
+#include "Ball.h"
 #include "Enemy.h"
 
 // Sets default values
@@ -12,6 +13,8 @@ AEnemy::AEnemy()
 	BoxCollider->SetupAttachment(GetRootComponent());
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	Mesh->OnComponentHit.AddDynamic(this, &AEnemy::OnBeginHit);
 }
 
 // Called when the game starts or when spawned
@@ -25,5 +28,19 @@ void AEnemy::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 	SetActorLocation(GetActorLocation() + (FVector(moveSpeed, 0, 0)*DeltaTime));
+}
+
+
+void AEnemy::OnBeginHit(class UPrimitiveComponent* HitComp, class AActor* OtherActor,
+                               class UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	if(OtherActor->ActorHasTag("Ball"))
+	{
+		ABall* OtherBall = Cast<ABall>(OtherActor);
+		if(OtherBall->bIsActive)
+		{
+			this->Destroy();
+		}
+	}
 }
 
