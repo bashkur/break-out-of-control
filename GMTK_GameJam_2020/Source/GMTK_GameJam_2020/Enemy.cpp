276df3db72dda3644af64ca6748eaa4e1fa0c242
Jefferey S <gods754513@gmail.com>
2020-07-13 01:15:13 -0400

    Finished Initial Build and QofL changes
    /*
    TL;DR
    	I did the build for the game. So we have a functional... well, saying it's a demo would be an overstep:
    	the QnE is that the castle doesn't do collisions and the wizard shield just doesn't show up. So the balls fly off and the Enemies don't seem to move forward. This is only in the build, the standalone_game mode and simulate work just fine in-editor. 
    	
    	There is a successful build tho, so that's a bug step.
    	
    	Everything that had a texture made for it is now added to the scene and thier associated meshes. Some quality of life sorting has been done (our organization is a disaster). And fixed some lighting, spacing, enviroment stuff for the GameLevel (MenuLevel is not implamented or fixed, we can touch on that later).
    	
    Changes:
    	- In Enemy.cpp:
    		changed included order so that Enemy.h was #included first
    	- Created our initial "shipping" build of the game
    	- In CastleBP
    		changed the static mesh component to the newer CastleMesh with fixed UVs
    		Changed the static mesh's material to the Phalanx metal red (it looks like a cheap metal toy (kinda))
    	- In GameLevel map
    		Changed the rotation of the back corner of pines so that they weren't all at a perfect 45 degrees
    		Changed the angle of the directional lighting (IT DID SO MUCH FOR THE LOOK OF THE GAME)
    	- Moved 'GameLevel" Unreal Map to the Maps folder in the Content Directory
    	- Changed the StormBall's particle Emitter (located in Jeffs_Shader_Emporium/Storm_Ball_Src) to have a shorter lifetime
    	- Added, on BP_Character in GameLevel map, activated "auto recieve input" for player0 (us)(duh)
    	- Moved EnemyManagerBP into the Blueprints folder (I haven't removed BP_EnemyManager because I don't know if we still use it)
    	- Added the materials to the Phalanx Mesh inside of FinalizingFolder/ToScale_Meshes
    	- Changed the horizontal spacing of the BP_Phalanx in EnemyManagerBP so that they're not overlapping
    	- Moved the ToScale_ BPs to the Blueprints folder
    	- changed the BP_Enemy in our blueprints folder to have the smallrock as its mesh to make things less confusing
    	
    Things I noticed, and probably should be fixed:
    	- I noticed BP_Phalanx references BP_Enemy, which are both using the Phalanx mesh... this is incredibly confusing for a level designer. We should probably clean this up
    	- There is two instances of the Phalanx Model in our game. One in FinalizeFolder/ToScale_Meshes and one in our Assets/Meshes. This is almost certianly my fault and I'll make a branch after this commit to address our organization

--- GMTK_GameJam_2020/Source/GMTK_GameJam_2020/Enemy.cpp
+++ GMTK_GameJam_2020/Source/GMTK_GameJam_2020/Enemy.cpp
@@ -1,8 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
-#include "Ball.h"
 #include "Enemy.h"
+#include "Ball.h"
 
 // Sets default values
 AEnemy::AEnemy()
